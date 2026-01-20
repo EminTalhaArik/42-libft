6e15d44647264388d5f27c91f08a5df88de64f5f
Emin Talha Arik <emiarik@k1m13s06.42kocaeli.com.tr>
2026-01-20 22:58:18 +0300

    Sanırım bişileri bozduk hojam

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: emiarik <emiarik@student.42kocaeli.com.tr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 17:49:18 by emiarik           #+#    #+#             */
-/*   Updated: 2026/01/17 19:27:18 by emiarik          ###   ########.fr       */
+/*   Updated: 2026/01/20 22:57:30 by emiarik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,7 @@ int	ft_isalnum(unsigned int character);
 int	ft_isascii(unsigned int character);
 int	ft_isprint(unsigned int character);
 int	ft_strlen(char *characters);
+
+void	ft_memset(void *ptr, int c, size_t len);
 
 #endif
