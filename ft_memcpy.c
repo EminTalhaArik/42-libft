567cdc9ce3f0ca5b3f1a1c0770c9b3ae263d16d3
Emin Talha Arik <emiarik@k2m15s06.42kocaeli.com.tr>
2026-01-25 18:43:09 +0300

    memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,9 +6,28 @@
 /*   By: emiarik <emiarik@student.42kocaeli.com.tr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 20:18:36 by emiarik           #+#    #+#             */
-/*   Updated: 2026/01/24 20:18:57 by emiarik          ###   ########.fr       */
+/*   Updated: 2026/01/25 18:42:56 by emiarik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char		*dst_org;
+	const unsigned char	*src_org;
+	size_t				i;
+
+	dst_org = (unsigned char *) dst;
+	src_org = (const unsigned char *) src;
+	if (dst_org == NULL && src_org == NULL)
+		return (dst);
+	i = 0;
+	while (i < n)
+	{
+		dst_org[i] = src_org[i];
+		i++;
+	}
+	return (dst);
+}
