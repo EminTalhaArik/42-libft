4258905b5749a7b5bc83b41a7c8d4874f97dc512
Emin Talha Arik <emiarik@k1m13s03.42kocaeli.com.tr>
2026-01-30 17:22:37 +0300

    strlcat, I modified my old strlcat function for new libft.h library

---
+++ ft_strlcat.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emiarik <emiarik@student.42kocaeli.com.tr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/30 17:14:18 by emiarik           #+#    #+#             */
+/*   Updated: 2026/01/30 17:22:01 by emiarik          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t n)
+{
+	size_t	dest_length;
+	size_t	src_length;
+	size_t	i;
+
+	dest_length = ft_strlen(dest);
+	src_length = ft_strlen((char *)src);
+	i = 0;
+	if (n <= dest_length)
+	{
+		return (n + src_length);
+	}
+	while (src[i]
+		&& dest_length + i + 1 < n)
+	{
+		dest[dest_length + 1] = src[i];
+		i++;
+	}
+	dest[dest_length + i] = '\0';
+	return (dest_length + src_length);
+}
