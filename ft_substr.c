4193d088984ccf578f845458f95fae698e842035
Emin Talha Arik <emiarik@k1m13s07.42kocaeli.com.tr>
2026-02-01 02:36:51 +0300

    .

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: emiarik <emiarik@student.42kocaeli.com.tr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 02:25:29 by emiarik           #+#    #+#             */
-/*   Updated: 2026/02/01 02:35:39 by emiarik          ###   ########.fr       */
+/*   Updated: 2026/02/01 02:36:41 by emiarik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,4 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	ft_memcpy(str, &s[start], len);
 	return (str);
-}
-
-#include <stdio.h>
-
-int	main(void)
-{
-	char	*selam;
-
-	selam = "merhaba hojam";
-	printf("%s", ft_substr(selam, 8, 5));
 }
