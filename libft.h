d0e3df1aac158de62285c1ad0479470666264cf6
Emin Talha Arik <emiarik@k1m20s03.42kocaeli.com.tr>
2026-02-08 05:13:02 +0300

    libft.h revize

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: emiarik <emiarik@student.42kocaeli.com.tr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 17:49:18 by emiarik           #+#    #+#             */
-/*   Updated: 2026/02/08 03:43:46 by emiarik          ###   ########.fr       */
+/*   Updated: 2026/02/08 05:12:48 by emiarik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+char	*ft_strtrim(char const *s1, char const *set);
 int		ft_toupper(int character);
 int		ft_tolower(int character);
 int		ft_strncmp(const char *s1, const char *s2, unsigned int n);
@@ -42,13 +43,32 @@ int		ft_isascii(int character);
 int		ft_isprint(int character);
 size_t	ft_strlen(const char *characters);
 size_t	ft_strlcpy(char *dest, const char *src, size_t n);
+size_t	ft_strlcat(char *dest, const char *src, size_t n);
+
 void	*ft_memset(void *selam, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
+
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+
+int		ft_lstsize(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_listiter(t_list *lst, void (*f)(void *));
+void	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
+t_list	*ft_lstnew(void *content);
+t_list	*ft_lstlast(t_list *last);
 
 #endif
