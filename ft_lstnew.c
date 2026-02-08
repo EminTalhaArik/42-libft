2ffef0a33f0a3c3dc52034987b4b23ebcd754d14
Emin Talha Arik <emiarik@k1m20s03.42kocaeli.com.tr>
2026-02-08 06:04:48 +0300

    lst revize 1

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emiarik <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/08 05:25:51 by emiarik           #+#    #+#             */
+/*   Updated: 2026/02/08 06:04:27 by emiarik          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
@@ -9,9 +20,7 @@ t_list	*ft_lstnew(void *content)
 	new_node = (t_list *)malloc(sizeof(t_list));
 	if (!new_node)
 		return (NULL);
-
 	new_node->content = content;
 	new_node->next = NULL;
-
 	return (new_node);
 }
