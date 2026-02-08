a8756df2df14f1657ef7b6185fe65bc4dc2a0791
Emin Talha Arik <emiarik@k1m20s03.42kocaeli.com.tr>
2026-02-08 15:18:30 +0300

    headers update

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: emiarik <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 07:01:05 by emiarik           #+#    #+#             */
-/*   Updated: 2026/02/08 07:01:10 by emiarik          ###   ########.fr       */
+/*   Updated: 2026/02/08 07:01:55 by emiarik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,38 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*))
 		return ;
 	del(lst->content);
 	free(lst);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	
+	t_list	*selam;
+	t_list	*selam2;
+	int		*a;
+	int		*b;	
+
+	a = ft_calloc(1, sizeof(int));
+	b = ft_calloc(1, sizeof(int));
+	
+	*a = 42;
+	*b = 21;
+	
+	selam = ft_lstnew(a);
+	selam2 = ft_lstnew(b);
+	selam->next = selam2;
+	t_list	*tmp;
+	tmp = selam;
+	while (tmp != NULL)
+	{
+		printf("%d \n", *(int *)(tmp->content));
+		tmp = tmp->next;
+	}
+
+	ft_lstclear(&selam, free);
+	if (selam == NULL)
+		printf("BASARILI! Liste tamamen silindi (selam == NULL).\n");
+	else
+		printf("HATA! Liste hala bir yeri gösteriyor: %p\n", (void *)selam);
 }
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emiarik <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/08 15:16:18 by emiarik           #+#    #+#             */
+/*   Updated: 2026/02/08 15:17:07 by emiarik          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
@@ -8,22 +19,18 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	size_t	i;
 	size_t	len;
 
-	if (!s || !f) 
+	if (!s || !f)
 		return (NULL);
-
-	len = ft_strlen(s); 
-
+	len = ft_strlen(s);
 	new_str = (char *)malloc(sizeof(char) * (len + 1));
-	if (!new_str) 
+	if (!new_str)
 		return (NULL);
-
 	i = 0;
 	while (i < len)
 	{
 		new_str[i] = f(i, s[i]);
 		i++;
 	}
-	new_str[i] = '\0'; 
-
+	new_str[i] = '\0';
 	return (new_str);
 }
